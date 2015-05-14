bd759702561f14b0b1e9693805ced99b741c3836
U-PS14B22\vantan <akira206@gmail.com>
2015-05-14 23:47:12 +0900

    リファ:

--- _5_14/UnityChanSample.cs
+++ _5_14/UnityChanSample.cs
@@ -2,88 +2,92 @@
 using System.Collections;
 
 public class UnityChanSample : MonoBehaviour {
+
   private Animator _animator;
-  private int _restHash = Animator.StringToHash("Rest"); // Hash化
+  private int _restHash  = Animator.StringToHash("Rest");
   private int _speedHash = Animator.StringToHash("Speed");
-  private int _jumpHash = Animator.StringToHash("Jump");
-  private int _timer = 0;
+  private int _jumpHash  = Animator.StringToHash("Jump");
+  private int _timer = 0; /* 待機モーションに入るまでのカウント
+                             180フレームを超えるとモーション開始 */
 
   void Start() {
     _animator = GetComponent<Animator>();
-    //animator.SetFloat("Speed", -0.2f);
-    //_animator.SetBool(_restHash, true); // Rest開始
   }
 
   void Update() {
-    if (Input.GetKey(KeyCode.UpArrow)) // 上キーが押されていたら
-    {
-      _animator.SetFloat(_speedHash, 0.6f);
-      transform.Translate(0, 0, 0.03f); // 前
-    } else if (Input.GetKey(KeyCode.DownArrow)) // 下キーが押されていたら
-    {
-      _animator.SetFloat(_speedHash, -0.3f);
-      transform.Translate(0, 0, -0.01f); // 後
-    }
+    Mover();
+    Rotator();
+    Jumper();
+    AnimMgr();
+  }
 
-    if (Input.GetKey(KeyCode.RightArrow)) // 右キーが押されていたら
-    {
-      transform.Rotate(0, 1.6f, 0); // 右回転
-    }
-    if (Input.GetKey(KeyCode.LeftArrow)) // 左キーが押されていたら
-    {
-      transform.Rotate(0, -1.6f, 0); // 左回転
-    }
+  void Mover() {
+    const float Move_Speed = 0.03f;
 
-    if (Input.GetKey(KeyCode.Space)) // スペースキーを押したら
-    {
-      _animator.SetBool(_jumpHash, true);
-    }
+    if (Input.GetKey(KeyCode.UpArrow)) {
+      transform.Translate(0, 0, Move_Speed);
+    } else
+      if (Input.GetKey(KeyCode.DownArrow)) {
+        transform.Translate(0, 0, -Move_Speed * 0.5f);
+      }
+  }
 
+  void Rotator() {
+    const float Rotate_Speed = 1.6f;
+
+    if (Input.GetKey(KeyCode.RightArrow)) {
+      transform.Rotate(0, Rotate_Speed, 0);
+    } else
+      if (Input.GetKey(KeyCode.LeftArrow)) {
+        transform.Rotate(0, -Rotate_Speed, 0);
+      }
+  }
+
+  void Jumper() {
+    if (Input.GetKey(KeyCode.Space)) {
+      _animator.SetBool(_jumpHash, true); // Jump開始
+    }
+  }
+
+  void AnimMgr() {
     AnimatorStateInfo info = _animator.GetCurrentAnimatorStateInfo(0);
+    const float Anim_Speed = 0.8f;
+
+    // ジャンプしたらfalseに戻す
     if (info.shortNameHash == _jumpHash) {
-      _animator.SetBool(_jumpHash, false);
+      _animator.SetBool(_jumpHash, false); // Jump停止
     }
 
-    // モーションを止める処理
-    // キーが離されたら
-    if (Input.GetKeyUp(KeyCode.UpArrow)) {
-      _animator.SetFloat(_speedHash, 0.0f);
-    } else if (Input.GetKeyUp(KeyCode.DownArrow)) {
-      _animator.SetFloat(_speedHash, 0.0f);
+    // 移動中のモーション
+    if (Input.GetKey(KeyCode.UpArrow)) {
+      _animator.SetFloat(_speedHash, Anim_Speed);
+    }
+    else if (Input.GetKeyUp(KeyCode.UpArrow)) {
+      _animator.SetFloat(_speedHash, 0.0f); // Anim停止
+    }
+    if (Input.GetKey(KeyCode.DownArrow)) {
+      _animator.SetFloat(_speedHash, -Anim_Speed * 0.5f);
+    }
+    else if (Input.GetKeyUp(KeyCode.DownArrow)) {
+      _animator.SetFloat(_speedHash, 0.0f); // Anim停止
     }
 
+    // 何かKeyが押されていたら
     if (Input.anyKey) {
       _timer = 0;
-    } else {
-      _animator.SetBool(_jumpHash, false);
     }
+    else {
+      _animator.SetBool(_jumpHash, false); // Jump停止
 
-    if (info.shortNameHash != _speedHash) {
       _timer++;
       if (_timer >= 180) {
-        _animator.SetBool(_restHash, true);
+        _animator.SetBool(_restHash, true); // 待機モーション開始
         _timer = 0;
       }
     }
 
-    if (info.shortNameHash == _restHash) // Animatorの状態がRestかどうか
-    {
-      _animator.SetBool(_restHash, false); // Rest終了
-      _animator.SetBool(_jumpHash, false); // Jump終了
+    if (info.shortNameHash == _restHash) {
+      _animator.SetBool(_restHash, false); // 待機モーション停止
     }
-
-    //AnimatorStateInfo info = _animator.GetCurrentAnimatorStateInfo(0);
-    // if (info.IsName("Rest"))
-    //if (info.shortNameHash == _restHash) // Animatorの状態がRestかどうか
-    //{
-    //  _animator.SetBool(_restHash, false); // Rest終了
-    //}
-
-    //transform.Translate(0.01f, 0, 0); // X方向に移動
-    //transform.Translate(0, 0.01f, 0); // Y方向に移動
-    //transform.Translate(0, 0, 0.01f); // Z方向に移動
-    //transform.Rotate(0, 1, 0); // Y軸の回転
-    //transform.Rotate(1, 0, 0); // X軸で回転
-    //transform.Rotate(0, 0, 1); // Z軸で回転
   }
 }
