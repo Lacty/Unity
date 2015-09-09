aa7dab161fedc97c3cf69c4e88adc99591f2ac60
Lacty <akira206@gmail.com>
2015-09-09 20:05:57 +0900

    Tower追加

--- OpenWorld/Assets/Script/GameCamera.cs
+++ OpenWorld/Assets/Script/GameCamera.cs
@@ -7,41 +7,41 @@ public class GameCamera : MonoBehaviour {
 
   [SerializeField]
   GameObject _player;
+  
+  [SerializeField]
+  float _offsetY;
 	
-	[SerializeField]
-	float _offsetY;
-	
-	Vector3 _direction;
+  Vector3 _direction;
 
   void Awake() {}
 
-	void Start() {
-		transform.LookAt(_player.GetComponent<Player>().GetTargetPos());
-	}
+  void Start() {
+    transform.LookAt(_player.GetComponent<Player>().GetTargetPos());
+  }
 	
-	void Update() {
-		SeekDirection();
-	  Move();
-		SetLookAt();
-	}
+  void Update() {
+    SeekDirection();
+    Move();
+    SetLookAt();
+  }
 	
-	void Move() {
-		Vector3 offset = new Vector3(0, _offsetY, 0);
-		Vector3 translate = _player.transform.position - _player.GetComponent<Player>().LastPos;
-		//transform.position += translate;
-		transform.position = _player.transform.position + offset - _direction * 6;
-		_player.GetComponent<Player>().LastPos = _player.transform.position;
-	}
+  void Move() {
+    Vector3 offset = new Vector3(0, _offsetY, 0);
+    Vector3 translate = _player.transform.position - _player.GetComponent<Player>().LastPos;
+    //transform.position += translate;
+    transform.position = _player.transform.position + offset - _direction * 6;
+    _player.GetComponent<Player>().LastPos = _player.transform.position;
+  }
 	
-	void SeekDirection() {
-		if (!_player.GetComponent<Player>().IsTarget()) return;
-		Vector3 offset = new Vector3(0, _offsetY, 0);
-		_direction = _player.GetComponent<Player>().GetTargetPos() - _player.transform.position + offset;
-		_direction.Normalize();
-	}
+  void SeekDirection() {
+    if (!_player.GetComponent<Player>().IsTarget()) return;
+    Vector3 offset = new Vector3(0, _offsetY, 0);
+    _direction = _player.GetComponent<Player>().GetTargetPos() - _player.transform.position + offset;
+    _direction.Normalize();
+  }
 	
-	void SetLookAt() {
-		if (!_player.GetComponent<Player>().IsTarget()) return;
-		transform.LookAt(_player.GetComponent<Player>().GetTargetPos());
-	}
+  void SetLookAt() {
+    if (!_player.GetComponent<Player>().IsTarget()) return;
+    transform.LookAt(_player.GetComponent<Player>().GetTargetPos());
+  }
 }
---
+++ OpenWorld/Assets/Script/Tower.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+
+public class Tower : MonoBehaviour {
+
+  [SerializeField]
+  private int _life;
+  public int Life {
+    get { return _life; }
+    set { _life = value; }
+  }
+
+  void Start () {}
+	
+  void Update () {}
+}
