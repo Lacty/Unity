371d5962d97b42e66032aed713013e5db1e7ee5d
Lacty <akira206@gmail.com>
2015-09-09 19:50:15 +0900

    OpenWorld新規追加

---
+++ OpenWorld/Assets/Script/GameCamera.cs
@@ -0,0 +1,47 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+
+public class GameCamera : MonoBehaviour {
+
+  [SerializeField]
+  GameObject _player;
+	
+	[SerializeField]
+	float _offsetY;
+	
+	Vector3 _direction;
+
+  void Awake() {}
+
+	void Start() {
+		transform.LookAt(_player.GetComponent<Player>().GetTargetPos());
+	}
+	
+	void Update() {
+		SeekDirection();
+	  Move();
+		SetLookAt();
+	}
+	
+	void Move() {
+		Vector3 offset = new Vector3(0, _offsetY, 0);
+		Vector3 translate = _player.transform.position - _player.GetComponent<Player>().LastPos;
+		//transform.position += translate;
+		transform.position = _player.transform.position + offset - _direction * 6;
+		_player.GetComponent<Player>().LastPos = _player.transform.position;
+	}
+	
+	void SeekDirection() {
+		if (!_player.GetComponent<Player>().IsTarget()) return;
+		Vector3 offset = new Vector3(0, _offsetY, 0);
+		_direction = _player.GetComponent<Player>().GetTargetPos() - _player.transform.position + offset;
+		_direction.Normalize();
+	}
+	
+	void SetLookAt() {
+		if (!_player.GetComponent<Player>().IsTarget()) return;
+		transform.LookAt(_player.GetComponent<Player>().GetTargetPos());
+	}
+}
---
+++ OpenWorld/Assets/Script/Player.cs
@@ -0,0 +1,59 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+
+public class Player : MonoBehaviour {
+
+  bool _is_target;
+	
+	Vector3 _target;
+	Vector3 _direction;
+	
+	[SerializeField]
+	Vector3 _speed;
+	Vector3 _velocity;
+	
+	Vector3 _last_pos;
+	public Vector3 LastPos {
+		get { return _last_pos; }
+		set { _last_pos = value; }
+	}
+
+  void Awake() {
+		_is_target = true;
+		_target = Vector3.zero;
+		_direction = _target - transform.position;
+	}
+
+	void Start() {
+	  
+	}
+	
+	void Update() {
+	  Move();
+	}
+	
+	void Move() {
+		_direction = _target - transform.position;
+		transform.LookAt(_target);
+		
+		if (Input.GetKey(KeyCode.A)) {
+			_velocity.x -= _speed.x;
+		}
+		if (Input.GetKey(KeyCode.D)) {
+			_velocity.x += _speed.x;
+		}
+		
+		_velocity *= 0.95f;
+		transform.Translate(_velocity);
+	}
+	
+	public Vector3 GetTargetPos() {
+		return _target;
+	}
+	
+	public bool IsTarget() {
+		return _is_target;
+	}
+}
